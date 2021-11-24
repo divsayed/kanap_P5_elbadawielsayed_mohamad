fe9c9a12c746eb197466be14e3b84323c4405450
EL BADAWI EL SAYED Mohamad <momosayed1@gmail.com>
2021-11-24 20:47:05 +0100

    Initial commit

---
+++ front/js/product.js
@@ -0,0 +1,60 @@
+// const urlParams = new URLSearchParams(window.location.search);
+// const id = urlParams.get('id')
+// console.log(id)
+
+/*------------------------------*/
+
+
+var str = window.location.href;
+var url = new URL(str);
+var id = url.searchParams.get("id");
+//console.log(id);
+
+fetch('http://localhost:3000/api/products/' + id) 
+    .then(response => response.json())
+    .then(donnees => {
+         //console.log(donnees)
+         let image = document.createElement('img')
+         image.setAttribute('src', donnees.imageUrl)
+         image.setAttribute('alt', donnees.altTxt )
+         document.getElementsByClassName('item__img')[0].append(image)
+         document.querySelector('h1').append(donnees.name)
+         document.querySelector('#price').textContent = donnees.price
+         document.getElementById('description').textContent = donnees.description
+         
+         //selection de la couleur
+         donnees.colors.forEach(couleur => {
+           //console.log(couleur)
+           let option = document.createElement('option')
+           option.textContent = couleur
+           option.setAttribute('value', couleur)
+           document.getElementById('colors').append(option)
+         });
+
+
+         //ajouter au panier
+        let btn = document.getElementById('addToCart')
+
+        btn.addEventListener("click", () =>{
+        let panier = JSON.parse(localStorage.getItem("panier")) != null ? JSON.parse(localStorage.getItem("panier")) : [];
+        //   console.log(panier)
+          
+          let product = {
+            'id' : donnees._id, 
+            'quantity' : Number( document.getElementById('quantity').value),
+            'color' : document.getElementById('colors').value,
+            'image' : image.src,
+            'imageAlt' : image.alt, 
+            'name'  : donnees.name,
+            'price' : donnees.price 
+          }
+        console.log(product)
+          panier.push(product)
+          localStorage.setItem("panier", JSON.stringify(panier));
+
+        })  
+
+            })
+    .catch (error => alert("Erreur de la requête"));
+
+
---
+++ front/js/script.js
@@ -0,0 +1,35 @@
+//requête service web pour accéder à l'api
+fetch('http://localhost:3000/api/products')
+.then(respense => respense.json())
+.then(donnee => { 
+    //console.log(donnee)
+    donnee.forEach(Display => {
+        //console.log(Display);
+        let section = document.getElementById('items');
+        let a = document.createElement('a');
+        let article = document.createElement('article');
+        let image = document.createElement('img');
+        let h3 = document.createElement('h3');
+        let p = document.createElement('p');
+  
+        a.setAttribute('href', './product.html?id=' + Display. _id);
+
+        image.setAttribute('src', Display.imageUrl );
+        image.setAttribute('alt', Display.altTxt );
+
+        h3.setAttribute('class', 'productName');
+        h3.textContent = Display.name;
+
+        p.setAttribute('class', 'productDescription');
+        p.textContent = Display.description;
+
+        article.append(image, h3, p);
+        a.append(article);
+        section.append(a);    
+          
+              }); 
+              
+})
+.catch(Error => alert ("Erreur est survenu"));
+
+//const image = `<img src = "${hjhhhhihh}" `  //AltGR + 7 + Espace
