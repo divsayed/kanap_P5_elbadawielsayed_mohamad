3126ed23d09192419b2470c3b953690908578649
EL BADAWI EL SAYED Mohamad <momosayed1@gmail.com>
2021-12-23 11:52:51 +0100

    modif commit

--- front/js/confirm.js
+++ front/js/confirm.js
@@ -2,5 +2,6 @@
 // recupère l'id produit dans l'url
 const urlParams = new URLSearchParams(window.location.search);
 const orderid = urlParams.get('orderId')
+
 // afficher l'id de la commande
 document.querySelector('#orderId').textContent = orderid;
